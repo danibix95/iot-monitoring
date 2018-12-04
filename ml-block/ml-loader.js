2499144c8b78b7a3501457c7eb98dbae9e05f630
danibix95 <dani95bissoli@gmail.com>
2018-12-04 15:40:24 +0100

    Node-Red block properties defined.

--- ml-block/ml-loader.js
+++ ml-block/ml-loader.js
@@ -1,5 +1,3 @@
-/**/
-
 module.exports = function(RED) {
     function MLLoaderNode(config) {
         // set node commons functionalities
@@ -7,13 +5,39 @@ module.exports = function(RED) {
 
         // this node functionalities implementation
         let node = this;
+        node.mtype = config.mtype;
+        
+        loadModel(node);
         // input event => called every time a message arrives to this node
         node.on("input", function(msg) {
-            msg.payload = msg.payload.toLowerCase();
+            let prediction = applyModel(node, []);
 
+            // update the message
+            msg.payload = { prediction : prediction };
             // this function is necessary to let messages go ahead along the flow
             node.send(msg);
         });
     }
+
+    /** Check whether all the required details are available
+    *   and then load the specified machine learning model
+    */
+    function loadModel(node) {
+        console.log("fernihgb", node);
+        if (node.mtype && (node.url || node.file)) {
+            node.status({ fill: "green", shape: "dot", text: "Model loaded!" });
+        }
+        else {
+            node.status({ fill: "red", shape: "ring", text: "Model not loaded!" });
+        }
+    }
+
+    /** Apply the loaded model onto given data and return model prediction
+    *   
+    */
+    function applyModel(node, data) {
+        return 0;
+    }
+
     RED.nodes.registerType("ml-loader", MLLoaderNode);
 }
--- ml-block/test/ml-loader_specs.js
+++ ml-block/test/ml-loader_specs.js
@@ -1,6 +1,6 @@
-let should = require("should");
-let helper = require("node-red-node-test-helper");
-let lowerNode = require("../ml-loader.js");
+const should = require("should");
+const helper = require("node-red-node-test-helper");
+const mlLoaderNode = require("../ml-loader.js");
 
 helper.init(require.resolve('node-red'));
 
@@ -10,29 +10,38 @@ describe('ml-loader Node', function () {
     });
 
     it('should be loaded', function (done) {
-        let flow = [{ id: "n1", type: "ml-loader", name: "test name" }];
-        helper.load(lowerNode, flow, function () {
-        
-        let n1 = helper.getNode("n1");
-        n1.should.have.property('name', 'test name');
-        done();
+        let flow = [{ id: "n1", type: "ml-loader", name: "test-name" }];
+        helper.load(mlLoaderNode, flow, function () {
+            let n1 = helper.getNode("n1");
+            n1.should.have.property('name', 'test-name');
+            done();
         });
     });
 
-    it('should make payload lower case', function (done) {
+    it("should return a payload that contains the model output", function (done) {
         let flow = [
-            { id: "n1", type: "ml-loader", name: "test name",wires:[["n2"]] },
+            {
+                id: "n1",
+                type: "ml-loader",
+                name: "test-name",
+                mtype : { value : "tensorflow" },
+                msource : { value: "file", required: true},
+                url : { value: "http://google.com"},
+                file : {value: ""},
+                wires: [["n2"]]
+            },
             { id: "n2", type: "helper" }
         ];
 
-        helper.load(lowerNode, flow, function () {
+        helper.load(mlLoaderNode, flow, function () {
             let n2 = helper.getNode("n2");
             let n1 = helper.getNode("n1");
             n2.on("input", function (msg) {
-                msg.should.have.property('payload', 'uppercase');
+                msg.should.have.property("payload");
+                msg.payload.should.have.property("prediction").which.is.not.undefined();
                 done();
             });
-            n1.receive({ payload: "UpperCase" });
+            n1.receive({ payload: 0 });
         });
     });
 });
