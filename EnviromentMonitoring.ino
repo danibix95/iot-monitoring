e2aca7d686e6696faae237d6eaebe8d4d177065e
danibix95 <dani95bissoli@gmail.com>
2019-01-07 15:21:14 +0100

    Indentation updated.

--- EnviromentMonitoring.ino
+++ EnviromentMonitoring.ino
@@ -6,7 +6,6 @@
 #define DHTPIN 2     
 #define DHTTYPE DHT11   // DHT 11 
 
-
 //Variable definitions
 DHT dht(DHTPIN, DHTTYPE);
 int loud;
@@ -15,86 +14,76 @@ float hum;
 float temp;
 float light;
 int mode;
-//char in='0'; //1 On 0 Off     
 char rxChar= 0;        
 boolean isOn = false;
 boolean newData = false;
 boolean anomaly = false;
 
-
-//Functions
-
-
-void printHelp(void){
-  Serial.println("--- Command list: ---");
-  Serial.println("? -> Print this HELP");  
-  Serial.println("1 -> On  \"activate\"");
-  Serial.println("2 -> Off \"deactivate\"");  
-  }
-
-//--------------- loop ----------------------------------------------- 
-void senseEnv(){
-  if (Serial.available()){          
-    rxChar = Serial.read();            
-    Serial.flush();                    
+// Functions
+void printHelp(void) {
+    Serial.println("--- Command list: ---");
+    Serial.println("? -> Print this HELP");  
+    Serial.println("1 -> On  \"activate\"");
+    Serial.println("2 -> Off \"deactivate\"");  
+}
+ 
+//------------------------------ loop ------------------------------
+void senseEnv() {
+    if (Serial.available()) {       
+        rxChar = Serial.read();            
+        Serial.flush();                    
   
-  switch (rxChar) {
-    
-    case '1':
-	if (isOn == false){        
-         isOn=true;
-         mode=1;
-          Serial.println("Sensing On!!");
-	}
-        else Serial.println("Sensing flag is already On!");
-        break;
+        switch (rxChar) {
+            case '1':
+            	if (isOn == false){        
+                     isOn=true;
+                     mode=1;
+                      Serial.println("Sensing On!!");
+            	}
+                else Serial.println("Sensing flag is already On!");
+                break;
 
-    case '0':
-	if (isOn=true){       
-       isOn=false;
-       mode=2;
-          Serial.println("Sensing Off!!");
-	}
-        else Serial.println("Sensing is already Off!");
-        break;
-        
-    case 'a':
-    case 'A':                          
-	if (anomaly == false)  {      
-         anomaly=true;
-         Serial.println("Anomaly Detected!!");
-         //actAnomaly();
-         mode=3;
-         //isOn=false;
-	}
-       // else Serial.println("Anomaly flag is already On!");
-        break;
-    case 'k':
-    case 'K':                          
-	if (anomaly == true)  {      
-         anomaly=false;
-         mode=4;
-          Serial.println("Anomaly Solved!!");
-	}
-       // else Serial.println("Anomaly flag is already Off!");
-        break;
-        
-    case '?':                         
-        printHelp();                   
-        break;
-        
-    default:                           
-      Serial.print("'");
-      Serial.print((char)rxChar);
-      Serial.println("' is not a command!");
+            case '0':
+            	if (isOn=true) {       
+                   isOn=false;
+                   mode=2;
+                      Serial.println("Sensing Off!!");
+            	}
+                else Serial.println("Sensing is already Off!");
+                break;
+            
+            case 'a':
+            case 'A':                          
+            	if (anomaly == false) {      
+                    anomaly=true;
+                    Serial.println("Anomaly Detected!!");
+                    mode=3;
+            	}
+                // else Serial.println("Anomaly flag is already On!");
+                break;
+
+            case 'k':
+            case 'K':                          
+            	if (anomaly == true)  {      
+                    anomaly=false;
+                    mode=4;
+                    Serial.println("Anomaly Solved!!");
+            	}
+                break;
+                
+            case '?':                         
+                printHelp();                   
+                break;
+                
+            default:                           
+                Serial.print("'");
+                Serial.print((char)rxChar);
+                Serial.println("' is not a command!");
+        }   
     }
-  }
 }
 
-
-
-void analogWrite25k(int pin, int value)
-{
+void analogWrite25k(int pin, int value) {
     switch (pin) {
         case 9:
             OCR1A = value;
@@ -108,26 +97,18 @@ void analogWrite25k(int pin, int value)
     }
 }
 
-void actAnomaly(){
-  if (anomaly){    
-    analogWrite25k(10, 320);
-  //  for (;;) ;  // infinite loop
-   // delay(100);
-  //Check again the anomaly and decide if to continue to act or not
- // analogWrite25k(10, 0);
- // anomaly=false;
-}
-  else{
-  analogWrite25k(10, 0);
-  }
-    
+void actAnomaly() {
+    if (anomaly) {    
+        analogWrite25k(10, 320);
+    }
+    else{
+        analogWrite25k(10, 0);
+    }
 }                 
 
-
-void setup()
-{ 
-  
-      // Configure Timer 1 for PWM @ 25 kHz.
+void setup() {
+    // Fan configuration
+    // Configure Timer 1 for PWM @ 25 kHz.
     TCCR1A = 0;           // undo the configuration done by...
     TCCR1B = 0;           // ...the Arduino core library
     TCNT1  = 0;           // reset timer
@@ -142,56 +123,49 @@ void setup()
     pinMode( 9, OUTPUT);
     pinMode(10, OUTPUT);
     
-  pinMode(LED_BUILTIN, OUTPUT);
-  Wire.begin();
-  dht.begin();
-  Serial.begin(9600);
-  TSL2561.init();
-    /*for (int thisPin = 2; thisPin < 7; thisPin++) {
-    pinMode(thisPin, OUTPUT); */
-  Serial.println("<Arduino is ready>");
-  Serial.flush();       // Clear receive buffer.
-  printHelp(); 
-
-  
+    pinMode(LED_BUILTIN, OUTPUT);
+    Wire.begin();
+    dht.begin();
+    Serial.begin(9600);
+    TSL2561.init();
+    
+    Serial.println("<Arduino is ready>");
+    Serial.flush(); // Clear receive buffer.
+    printHelp();
 }
 
-void loop()
-{    
+void loop() {    
+    senseEnv();
+    if (isOn) {
+        actAnomaly();
+        hum = dht.readHumidity();
+        temp = dht.readTemperature();
+        loud = analogRead(0);
+        light=TSL2561.readVisibleLux();
+        loud = map(loud, 0, 1023, -48, 66);
 
+        const int capacity = JSON_ARRAY_SIZE(4) + 4 * JSON_OBJECT_SIZE(2);
+        StaticJsonBuffer<capacity> jb;
+        JsonArray& sensorData = jb.createArray();
 
-   senseEnv();
-    if(isOn){
-   actAnomaly();
-    hum = dht.readHumidity();
-    temp = dht.readTemperature();
-    loud = analogRead(0);
-    light=TSL2561.readVisibleLux();
-      loud = map(loud, 0, 1023, -48, 66);
-
-  
-  const int capacity = JSON_ARRAY_SIZE(4) + 4*JSON_OBJECT_SIZE(2);
-  StaticJsonBuffer<capacity> jb;
-  JsonArray& sensorData = jb.createArray();
-
-     JsonObject& obj1 = sensorData.createNestedObject();
-     obj1["key"] = "Humidity";
-     obj1["value"] = hum;
-     JsonObject& obj2 = sensorData.createNestedObject();
-     obj2["key"] = "Temperature";
-     obj2["value"] = temp;
-     JsonObject& obj3 = sensorData.createNestedObject();
-     obj3["key"] = "Loudness";
-     obj3["value"] = loud;
-     JsonObject& obj4 = sensorData.createNestedObject();
-     obj4["key"] = "Light";
-     obj4["value"] = light;
+        JsonObject& obj1 = sensorData.createNestedObject();
+        obj1["key"] = "Humidity";
+        obj1["value"] = hum;
+        JsonObject& obj2 = sensorData.createNestedObject();
+        obj2["key"] = "Temperature";
+        obj2["value"] = temp;
+        JsonObject& obj3 = sensorData.createNestedObject();
+        obj3["key"] = "Loudness";
+        obj3["value"] = loud;
+        JsonObject& obj4 = sensorData.createNestedObject();
+        obj4["key"] = "Light";
+        obj4["value"] = light;
    
-   sensorData.printTo(Serial);
-   Serial.println();
-delay(1000);
-
-}}
+        sensorData.printTo(Serial);
+        Serial.println();
+        delay(1000);
+    }
+}
 
 
  
